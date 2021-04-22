09bf0ae26407266a01fb7c2261c3445b1d52d0f1
reyansh <yash.dewangan82@gmail.com>
2021-04-22 14:38:24 +0100

    url02 update

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -7,7 +7,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/main/resources/Features", glue = {"StepDefinitions"},
-monochrome = true, plugin = {"pretty", "html:target/htmlReports"},
+monochrome = true,
+        //plugin = {"pretty", "html:target/htmlReports"},
         tags = "@all"
       )
 public class TestRunner {
